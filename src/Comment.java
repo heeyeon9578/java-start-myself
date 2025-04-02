5b991f234f81dfca05df8caf2f7e01805bcceb86
heeyeon9578 <heeyeon9578@naver.com>
2025-04-02 18:12:41 +0900

    처음 커밋

---
+++ src/Comment.java
@@ -0,0 +1,9 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java1 을 출력합니다. //sout
+        System.out.println("hello java2");
+        /*
+        여러줄 주석
+         */
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,7 @@
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("HEllo java1");
+        System.out.println("HEllo java2");
+        System.out.println("HEllo java3");
+    }
+}
